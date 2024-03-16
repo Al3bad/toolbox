796f7de14bff4d3b06adcf2ab576aab472f3f131
Abdullah Alabbad <abdullah@alabbad.dev>
2024-03-17 00:29:01 +1100

    fix resolving path for links

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
-// import "./style.css";
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = "";
+const base = document.createElement("base");
+base.href = import.meta.env.BASE_URL;
+document.head.insertBefore(base, document.head.firstChild);
