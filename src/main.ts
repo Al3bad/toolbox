219a0613de1ac74e8230a377895e5d94ff4214d6
Abdullah Alabbad <abdullah@alabbad.dev>
2024-03-20 10:36:03 +1100

    delete unused files / refactory $

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-export const $ = document.querySelector.bind(document)!;
+export const $ = (selector: string) => {
+  if (!selector) throw "Invalid Selector";
+  let elements = document.querySelectorAll(selector);
+  return elements.length === 1 ? elements[0] : elements;
+};
